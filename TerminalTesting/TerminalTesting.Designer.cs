c9b6bead3e65b58c928a60c6bc4c240e7598271e
Maugoncr <gonzalezmoralesmauricioo@gmail.com>
2024-04-23 09:48:43 -0600

    Add project files.

---
+++ TerminalTesting/TerminalTesting.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TerminalTesting
+{
+    partial class TerminalTesting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comPort = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnRefreshCOM = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSendCommandRequestTC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comPort
+            // 
+            this.comPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comPort.FormattingEnabled = true;
+            this.comPort.Location = new System.Drawing.Point(39, 62);
+            this.comPort.Name = "comPort";
+            this.comPort.Size = new System.Drawing.Size(121, 21);
+            this.comPort.TabIndex = 0;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.Parity = System.IO.Ports.Parity.Even;
+            this.serialPort1.WriteTimeout = 100;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // btnRefreshCOM
+            // 
+            this.btnRefreshCOM.Location = new System.Drawing.Point(39, 142);
+            this.btnRefreshCOM.Name = "btnRefreshCOM";
+            this.btnRefreshCOM.Size = new System.Drawing.Size(121, 23);
+            this.btnRefreshCOM.TabIndex = 1;
+            this.btnRefreshCOM.Text = "Refresh COM Ports";
+            this.btnRefreshCOM.UseVisualStyleBackColor = true;
+            this.btnRefreshCOM.Click += new System.EventHandler(this.btnRefreshCOM_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.Red;
+            this.btnConnect.Location = new System.Drawing.Point(39, 99);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(121, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSendCommandRequestTC
+            // 
+            this.btnSendCommandRequestTC.Location = new System.Drawing.Point(607, 62);
+            this.btnSendCommandRequestTC.Name = "btnSendCommandRequestTC";
+            this.btnSendCommandRequestTC.Size = new System.Drawing.Size(249, 54);
+            this.btnSendCommandRequestTC.TabIndex = 3;
+            this.btnSendCommandRequestTC.Text = "Send Request Command";
+            this.btnSendCommandRequestTC.UseVisualStyleBackColor = true;
+            this.btnSendCommandRequestTC.Click += new System.EventHandler(this.btnSendCommandRequestTC_Click);
+            // 
+            // TerminalTesting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(965, 499);
+            this.Controls.Add(this.btnSendCommandRequestTC);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnRefreshCOM);
+            this.Controls.Add(this.comPort);
+            this.Name = "TerminalTesting";
+            this.Text = "Terminal Testing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comPort;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnRefreshCOM;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSendCommandRequestTC;
+    }
+}
+
---
+++ TerminalTesting/TerminalTesting.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TerminalTesting
+{
+    public partial class TerminalTesting : Form
+    {
+        public TerminalTesting()
+        {
+            InitializeComponent();
+        }
+
+        private void SetConfigSerialPortForHeater()
+        {
+            serialPort1.DataBits = 8;
+            serialPort1.Parity = Parity.Even;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.WriteTimeout = 100;
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] puertos = SerialPort.GetPortNames();
+            comPort.Items.Clear();
+            comPort.Items.AddRange(puertos);
+        }
+
+        private void btnRefreshCOM_Click(object sender, EventArgs e)
+        {
+            string[] puertos = SerialPort.GetPortNames();
+            comPort.Items.Clear();
+            comPort.Items.AddRange(puertos);
+        }
+
+        private bool reconocerCOMForComponents(string COM)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                    return false;
+                }
+                // Remember Baud Rate
+                serialPort1.PortName = COM;
+                serialPort1.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (btnConnect.BackColor == Color.Red)
+            {
+                if (reconocerCOMForComponents(comPort.SelectedItem.ToString()))
+                {
+                    btnConnect.BackColor = Color.Green;
+                    btnRefreshCOM.Enabled = false;
+                }
+            }
+            else
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                    btnConnect.BackColor = Color.Red;
+                    btnRefreshCOM.Enabled = true;
+                }
+            }
+        }
+
+        private void sendRequestTCTemp()
+        {
+            SetConfigSerialPortForHeater();
+            string hexCommand;
+            string[] hexBytes;
+            byte[] binaryData;
+
+            hexCommand = "01 03 20 00 00 01 8F CA";
+
+            hexBytes = hexCommand.Split(' ');
+
+            binaryData = new byte[hexBytes.Length];
+
+            for (int i = 0; i < hexBytes.Length; i++)
+            {
+                binaryData[i] = Convert.ToByte(hexBytes[i], 16);
+            }
+            serialPort1.Write(binaryData, 0, binaryData.Length);
+        }
+
+        private void btnSendCommandRequestTC_Click(object sender, EventArgs e)
+        {
+            sendRequestTCTemp();
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+        }
+    }
+}
